33e978d42ca79c9390cd91e1096a253aa55524c5
Weiskri <kkweis13@gmail.com>
2018-02-05 10:02:20 -0500

    first commit

---
+++ DictionariesPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariesPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Syntax - Dictionary <key data type, value data type> dictionaryname
+            Dictionary<string, int> studentGrades = new Dictionary<string, int>()
+            {
+                {"Kerry", 90 }, // "kerry is the key", 90 is the value (her grade)
+                {"Barry", 85 },
+                {"Michelle", 100 }
+            }; 
+
+        }
+    }
+}
